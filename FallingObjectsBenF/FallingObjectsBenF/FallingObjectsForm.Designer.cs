cebeeb1cab74e9202eed2606d8b3f501e96fa5d8
Benjamin Falsetto <32547110+BenjaminFalsetto@users.noreply.github.com>
2018-09-19 10:39:59 -0400

    commit

--- FallingObjectsBenF/FallingObjectsBenF/FallingObjectsForm.Designer.cs
+++ FallingObjectsBenF/FallingObjectsBenF/FallingObjectsForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FallingObjectsBenF
 {
-    partial class Form1
+    partial class frmFallingObjects
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmFallingObjects
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(545, 339);
+            this.Name = "frmFallingObjects";
+            this.Text = "Falling Objects Ben F";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- FallingObjectsBenF/FallingObjectsBenF/FallingObjectsForm.cs
+++ FallingObjectsBenF/FallingObjectsBenF/FallingObjectsForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace FallingObjectsBenF
 {
-    public partial class Form1 : Form
+    public partial class frmFallingObjects : Form
     {
-        public Form1()
+        public frmFallingObjects()
         {
             InitializeComponent();
         }
