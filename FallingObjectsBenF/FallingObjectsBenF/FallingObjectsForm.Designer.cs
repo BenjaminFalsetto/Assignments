750fa8501d53d7c110375361f5aed34b84fe560a
Benjamin Falsetto <32547110+BenjaminFalsetto@users.noreply.github.com>
2018-09-25 10:46:49 -0400

    sdf

--- FallingObjectsBenF/FallingObjectsBenF/FallingObjectsForm.Designer.cs
+++ FallingObjectsBenF/FallingObjectsBenF/FallingObjectsForm.Designer.cs
@@ -43,29 +43,26 @@
             this.lblEnterTime.AutoSize = true;
             this.lblEnterTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblEnterTime.ForeColor = System.Drawing.Color.Red;
-            this.lblEnterTime.Location = new System.Drawing.Point(43, 96);
-            this.lblEnterTime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEnterTime.Location = new System.Drawing.Point(32, 78);
             this.lblEnterTime.Name = "lblEnterTime";
-            this.lblEnterTime.Size = new System.Drawing.Size(1014, 42);
+            this.lblEnterTime.Size = new System.Drawing.Size(807, 33);
             this.lblEnterTime.TabIndex = 0;
             this.lblEnterTime.Text = "Enter the time (in seconds) since you released the object";
             // 
             // txtEnterTime
             // 
             this.txtEnterTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtEnterTime.Location = new System.Drawing.Point(1144, 103);
-            this.txtEnterTime.Margin = new System.Windows.Forms.Padding(4);
+            this.txtEnterTime.Location = new System.Drawing.Point(858, 84);
             this.txtEnterTime.Name = "txtEnterTime";
-            this.txtEnterTime.Size = new System.Drawing.Size(89, 34);
+            this.txtEnterTime.Size = new System.Drawing.Size(68, 29);
             this.txtEnterTime.TabIndex = 1;
             // 
             // btnCalculate
             // 
             this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
-            this.btnCalculate.Location = new System.Drawing.Point(459, 297);
-            this.btnCalculate.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCalculate.Location = new System.Drawing.Point(344, 241);
             this.btnCalculate.Name = "btnCalculate";
-            this.btnCalculate.Size = new System.Drawing.Size(499, 69);
+            this.btnCalculate.Size = new System.Drawing.Size(374, 56);
             this.btnCalculate.TabIndex = 2;
             this.btnCalculate.Text = "CALCULATE!";
             this.btnCalculate.UseVisualStyleBackColor = true;
@@ -78,8 +75,7 @@
             this.mniFile});
             this.mnuFallingObjects.Location = new System.Drawing.Point(0, 0);
             this.mnuFallingObjects.Name = "mnuFallingObjects";
-            this.mnuFallingObjects.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
-            this.mnuFallingObjects.Size = new System.Drawing.Size(1485, 28);
+            this.mnuFallingObjects.Size = new System.Drawing.Size(1114, 24);
             this.mnuFallingObjects.TabIndex = 3;
             this.mnuFallingObjects.Text = "menuStrip1";
             // 
@@ -88,13 +84,13 @@
             this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.mniExit});
             this.mniFile.Name = "mniFile";
-            this.mniFile.Size = new System.Drawing.Size(44, 24);
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
             this.mniFile.Text = "File";
             // 
             // mniExit
             // 
             this.mniExit.Name = "mniExit";
-            this.mniExit.Size = new System.Drawing.Size(216, 26);
+            this.mniExit.Size = new System.Drawing.Size(92, 22);
             this.mniExit.Text = "Exit";
             this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
             // 
@@ -102,25 +98,23 @@
             // 
             this.lblAns.AutoSize = true;
             this.lblAns.Font = new System.Drawing.Font("MV Boli", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAns.Location = new System.Drawing.Point(33, 544);
-            this.lblAns.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAns.Location = new System.Drawing.Point(25, 442);
             this.lblAns.Name = "lblAns";
-            this.lblAns.Size = new System.Drawing.Size(1253, 45);
+            this.lblAns.Size = new System.Drawing.Size(994, 34);
             this.lblAns.TabIndex = 4;
             this.lblAns.Text = "The object has fallen x amount of meters since you released the object";
             // 
             // frmFallingObjects
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1485, 777);
+            this.ClientSize = new System.Drawing.Size(1114, 631);
             this.Controls.Add(this.lblAns);
             this.Controls.Add(this.btnCalculate);
             this.Controls.Add(this.txtEnterTime);
             this.Controls.Add(this.lblEnterTime);
             this.Controls.Add(this.mnuFallingObjects);
             this.MainMenuStrip = this.mnuFallingObjects;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmFallingObjects";
             this.Text = "Falling Objects Ben F";
             this.mnuFallingObjects.ResumeLayout(false);
