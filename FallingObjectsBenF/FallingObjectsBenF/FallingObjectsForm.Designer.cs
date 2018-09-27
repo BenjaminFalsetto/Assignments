41999640473dc01a1527d34fcb0dfeb0d59df724
BenjaminFalsetto <32547110+BenjaminFalsetto@users.noreply.github.com>
2018-09-26 21:01:36 -0400

    Completed assignment #2

--- FallingObjectsBenF/FallingObjectsBenF/FallingObjectsForm.Designer.cs
+++ FallingObjectsBenF/FallingObjectsBenF/FallingObjectsForm.Designer.cs
@@ -38,7 +38,7 @@
             this.lblAns = new System.Windows.Forms.Label();
             this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
             this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
-            this.lblError = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.mnuFallingObjects.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).BeginInit();
@@ -49,26 +49,30 @@
             this.lblEnterTime.AutoSize = true;
             this.lblEnterTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblEnterTime.ForeColor = System.Drawing.Color.Red;
-            this.lblEnterTime.Location = new System.Drawing.Point(32, 78);
+            this.lblEnterTime.Location = new System.Drawing.Point(34, 161);
+            this.lblEnterTime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblEnterTime.Name = "lblEnterTime";
-            this.lblEnterTime.Size = new System.Drawing.Size(807, 33);
+            this.lblEnterTime.Size = new System.Drawing.Size(1014, 42);
             this.lblEnterTime.TabIndex = 0;
             this.lblEnterTime.Text = "Enter the time (in seconds) since you released the object";
             // 
             // txtEnterTime
             // 
             this.txtEnterTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtEnterTime.Location = new System.Drawing.Point(858, 84);
+            this.txtEnterTime.Location = new System.Drawing.Point(1108, 168);
+            this.txtEnterTime.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtEnterTime.Name = "txtEnterTime";
-            this.txtEnterTime.Size = new System.Drawing.Size(68, 29);
+            this.txtEnterTime.Size = new System.Drawing.Size(89, 34);
             this.txtEnterTime.TabIndex = 1;
+            this.txtEnterTime.TextChanged += new System.EventHandler(this.txtEnterTime_TextChanged);
             // 
             // btnCalculate
             // 
             this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
-            this.btnCalculate.Location = new System.Drawing.Point(344, 241);
+            this.btnCalculate.Location = new System.Drawing.Point(459, 297);
+            this.btnCalculate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCalculate.Name = "btnCalculate";
-            this.btnCalculate.Size = new System.Drawing.Size(374, 56);
+            this.btnCalculate.Size = new System.Drawing.Size(499, 69);
             this.btnCalculate.TabIndex = 2;
             this.btnCalculate.Text = "CALCULATE!";
             this.btnCalculate.UseVisualStyleBackColor = true;
@@ -81,7 +85,8 @@
             this.mniFile});
             this.mnuFallingObjects.Location = new System.Drawing.Point(0, 0);
             this.mnuFallingObjects.Name = "mnuFallingObjects";
-            this.mnuFallingObjects.Size = new System.Drawing.Size(1114, 24);
+            this.mnuFallingObjects.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.mnuFallingObjects.Size = new System.Drawing.Size(1485, 28);
             this.mnuFallingObjects.TabIndex = 3;
             this.mnuFallingObjects.Text = "menuStrip1";
             // 
@@ -90,13 +95,13 @@
             this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.mniExit});
             this.mniFile.Name = "mniFile";
-            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Size = new System.Drawing.Size(44, 24);
             this.mniFile.Text = "File";
             // 
             // mniExit
             // 
             this.mniExit.Name = "mniExit";
-            this.mniExit.Size = new System.Drawing.Size(92, 22);
+            this.mniExit.Size = new System.Drawing.Size(108, 26);
             this.mniExit.Text = "Exit";
             this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
             // 
@@ -104,9 +109,10 @@
             // 
             this.lblAns.AutoSize = true;
             this.lblAns.Font = new System.Drawing.Font("MV Boli", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAns.Location = new System.Drawing.Point(25, 442);
+            this.lblAns.Location = new System.Drawing.Point(33, 544);
+            this.lblAns.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblAns.Name = "lblAns";
-            this.lblAns.Size = new System.Drawing.Size(994, 34);
+            this.lblAns.Size = new System.Drawing.Size(1253, 45);
             this.lblAns.TabIndex = 4;
             this.lblAns.Text = "The object has fallen x amount of meters since you released the object";
             // 
@@ -118,28 +124,32 @@
             // 
             this.errorProvider2.ContainerControl = this;
             // 
-            // lblError
+            // label1
             // 
-            this.lblError.AutoSize = true;
-            this.lblError.Location = new System.Drawing.Point(114, 187);
-            this.lblError.Name = "lblError";
-            this.lblError.Size = new System.Drawing.Size(29, 13);
-            this.lblError.TabIndex = 5;
-            this.lblError.Text = "Error";
-            this.lblError.Click += new System.EventHandler(this.label1_Click);
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(34, 51);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(915, 84);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "You are standing on a platform holding an object.\r\nThe object is exactly 100 mete" +
+    "rs above the ground.";
             // 
             // frmFallingObjects
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1114, 631);
-            this.Controls.Add(this.lblError);
+            this.ClientSize = new System.Drawing.Size(1485, 777);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.lblAns);
             this.Controls.Add(this.btnCalculate);
             this.Controls.Add(this.txtEnterTime);
             this.Controls.Add(this.lblEnterTime);
             this.Controls.Add(this.mnuFallingObjects);
             this.MainMenuStrip = this.mnuFallingObjects;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmFallingObjects";
             this.Text = "Falling Objects Ben F";
             this.mnuFallingObjects.ResumeLayout(false);
@@ -161,8 +171,8 @@
         private System.Windows.Forms.ToolStripMenuItem mniExit;
         private System.Windows.Forms.Label lblAns;
         private System.Windows.Forms.ErrorProvider errorProvider1;
-        private System.Windows.Forms.Label lblError;
         private System.Windows.Forms.ErrorProvider errorProvider2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- FallingObjectsBenF/FallingObjectsBenF/FallingObjectsForm.cs
+++ FallingObjectsBenF/FallingObjectsBenF/FallingObjectsForm.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/*
+ * Created by: Benjamin Falsetto
+ * Created on: 2018/09/25
+ * Created for: ICS3U Programming
+ * Weekly Assignment #2
+ * This program calculates the height an object is off
+ * of the ground when given the time since object released.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,54 +25,96 @@ namespace FallingObjectsBenF
         {
             InitializeComponent();
             this.lblAns.Hide();
-            this.lblError.Hide();
         }
 
         private void btnCalculate_Click(object sender, EventArgs e)
         {
             //declare local variables
-            double time = 5.14, height;
+            double time = Double.Parse(txtEnterTime.Text), height;
 
             //declare constant
             const double MAXTIME = 4.514;
 
+            //textbox numerical error checking
+
+            //looking to see if text box is empty
             if (txtEnterTime.Text.Trim() == string.Empty)
             {
+                //if the text box is empty, display a message box onscreen -
+                //telling the user that the text box is empty
                 MessageBox.Show("Please enter something in the textbox");
             }
-            /*else if (txtEnterTime < MAXTIME)
+
+            //looking to see if the entered time exceeds the time that it takes for the -
+            //object to hit the ground
+            if (time > MAXTIME)
+            {
+                //if the time exceeds MAXTIME, display a message box onsxreen -
+                //telling the user that the object has already hit the ground
+                MessageBox.Show("The object has already hit the ground");
+            }
+            //checking to see if the time is outside certain parameters
+            if (time <= 0 && time >= 4.514)
+            {
+                //if the time is outside certain parameters, tell the user to -
+                //input a time value within the given parameters
+                MessageBox.Show("Error. Please enter a number between 0 and 4.514");
+            }
+            //looling to see if the time is less than 0
+            if (time < 0)
+            {
+                //if the time is less than 0, tell the user to enter a valid value
+                MessageBox.Show("Error. Please enter a number between 0 and 4.514");
+            }
+            //check to see if the time is 0
+            if (time == 0)
+            {
+                //if true, tell the user that the object hasn't been released yet
+                MessageBox.Show("The object hasn't been released yet");
+            }
+            //checking to see if time is within certain parameters
+            if (time >= 0.0000000001 && time <= 4.514)
+            {
+                //if true, display the answer label
+                //otherwise it would display values that don't make sense
+                this.lblAns.Show();
+            }
+
                 //convert the string from the text box to a double
                 time = Double.Parse(txtEnterTime.Text);
-
-                //calculate the height
-                height = 100 - (0.5 * 9.81 * Math.Pow(time, 2));
-
-                //insert the answer into it's label
-                this.lblAns.Text = Convert.ToString(height) + " meters";
-            */
-            if(time < 0)
-                MessageBox.Show("The object hasn't been re")
-
-            //convert the string from the text box to a double
-            time = Double.Parse(txtEnterTime.Text);
             
             //calculate the height
             height = 100 - (0.5 * 9.81 * Math.Pow(time, 2));
 
-            //insert the answer into it's label
-            this.lblAns.Text = Convert.ToString(height) + " meters";
-            this.lblAns.Show();
-            
+            //insert the answer into it's label and then show the variable
+            this.lblAns.Text ="The object has fallen " + Convert.ToString(height) + " meters since you released it " + time + " seconds ago";
+
         }
 
         private void mniExit_Click(object sender, EventArgs e)
         {
+            //if mniExit is clicked, exit the program
             this.Close();
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void txtEnterTime_TextChanged(object sender, EventArgs e)
+        {
+            //textbox alphabetical error checking
+
+            //checking to see if a letter was inputed
+            if (System.Text.RegularExpressions.Regex.IsMatch(txtEnterTime.Text, "[^0-9]"))
+            {
+                //if a letter was inputed, tell the user that only numbers are accepted
+                MessageBox.Show("Please enter only numbers.");
+                
+                //clear the textbox if a letter was inputed
+                txtEnterTime.Text = txtEnterTime.Text.Remove(txtEnterTime.Text.Length - 1);
+            }
         }
     }
 }
