0d3ba127bc2e557dbd380064c95ba2be47d7b2f4
Benjamin Falsetto <32547110+BenjaminFalsetto@users.noreply.github.com>
2018-10-19 18:35:36 -0400

    Update

---
+++ PizzaOrderBenF/PizzaOrderBenF/PizzaOrderForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace PizzaOrderBenF
+{
+    partial class frmPizzaOrder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.mnuOptions = new System.Windows.Forms.MenuStrip();
+            this.SuspendLayout();
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeader.Location = new System.Drawing.Point(33, 37);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(326, 25);
+            this.lblHeader.TabIndex = 0;
+            this.lblHeader.Text = "Generic pizzeria but with an app.";
+            // 
+            // mnuOptions
+            // 
+            this.mnuOptions.Location = new System.Drawing.Point(0, 0);
+            this.mnuOptions.Name = "mnuOptions";
+            this.mnuOptions.Size = new System.Drawing.Size(941, 24);
+            this.mnuOptions.TabIndex = 1;
+            this.mnuOptions.Text = "menuStrip1";
+            // 
+            // frmPizzaOrder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 497);
+            this.Controls.Add(this.lblHeader);
+            this.Controls.Add(this.mnuOptions);
+            this.MainMenuStrip = this.mnuOptions;
+            this.Name = "frmPizzaOrder";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.MenuStrip mnuOptions;
+    }
+}
+
