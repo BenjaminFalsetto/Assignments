e8d9ed2e05ba1d62735ae174d5a1a1c97be88928
Benjamin Falsetto <32547110+BenjaminFalsetto@users.noreply.github.com>
2018-10-19 21:43:56 -0400

    Update

--- PizzaOrderBenF/PizzaOrderBenF/PizzaOrderForm.Designer.cs
+++ PizzaOrderBenF/PizzaOrderBenF/PizzaOrderForm.Designer.cs
@@ -154,9 +154,24 @@
             // numericUpDown1
             // 
             this.numericUpDown1.Location = new System.Drawing.Point(10, 36);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
             this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             // 
             // lblStep1Part1
             // 
