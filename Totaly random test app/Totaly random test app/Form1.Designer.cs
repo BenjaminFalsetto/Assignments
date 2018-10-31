cbf0ccbcdc67e7c88b0c06acf15dbf1760189c0a
Benjamin Falsetto <32547110+BenjaminFalsetto@users.noreply.github.com>
2018-10-31 10:12:38 -0400

    RandomCommit

--- Totaly random test app/Totaly random test app/Form1.Designer.cs
+++ Totaly random test app/Totaly random test app/Form1.Designer.cs
@@ -28,34 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnLogIn = new System.Windows.Forms.Button();
+            this.lblEnterUsername = new System.Windows.Forms.Label();
+            this.lblEnterPass = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // btnLogIn
             // 
-            this.button1.Location = new System.Drawing.Point(102, 72);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnLogIn.Location = new System.Drawing.Point(98, 141);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(75, 23);
+            this.btnLogIn.TabIndex = 0;
+            this.btnLogIn.Text = "Log In";
+            this.btnLogIn.UseVisualStyleBackColor = true;
+            this.btnLogIn.Click += new System.EventHandler(this.btnLogIn_Click);
+            // 
+            // lblEnterUsername
+            // 
+            this.lblEnterUsername.AutoSize = true;
+            this.lblEnterUsername.Location = new System.Drawing.Point(32, 57);
+            this.lblEnterUsername.Name = "lblEnterUsername";
+            this.lblEnterUsername.Size = new System.Drawing.Size(83, 13);
+            this.lblEnterUsername.TabIndex = 1;
+            this.lblEnterUsername.Text = "Enter Username";
+            // 
+            // lblEnterPass
+            // 
+            this.lblEnterPass.AutoSize = true;
+            this.lblEnterPass.Location = new System.Drawing.Point(34, 83);
+            this.lblEnterPass.Name = "lblEnterPass";
+            this.lblEnterPass.Size = new System.Drawing.Size(81, 13);
+            this.lblEnterPass.TabIndex = 2;
+            this.lblEnterPass.Text = "Enter Password";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(121, 54);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 20);
+            this.txtUsername.TabIndex = 3;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(121, 80);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '1';
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 4;
+            this.txtPassword.UseSystemPasswordChar = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(284, 207);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lblEnterPass);
+            this.Controls.Add(this.lblEnterUsername);
+            this.Controls.Add(this.btnLogIn);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnLogIn;
+        private System.Windows.Forms.Label lblEnterUsername;
+        private System.Windows.Forms.Label lblEnterPass;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
     }
 }
 
--- Totaly random test app/Totaly random test app/Form1.cs
+++ Totaly random test app/Totaly random test app/Form1.cs
@@ -13,19 +13,31 @@ namespace Totaly_random_test_app
     public partial class Form1 : Form
     {
         Form2 frm2;
-
+        string Username = "TESTUSER", Password = "TESTPASS";
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btnLogIn_Click(object sender, EventArgs e)
         {
-            frm2 = new Form2();
-            this.Hide();
-            frm2.ShowDialog();
-            this.Close();
+            if (txtUsername.Text == Username && txtPassword.Text == Password)
+            {
+                MessageBox.Show("Logged In!");
+                frm2 = new Form2();
+                this.Hide();
+                frm2.ShowDialog();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Incorrect Username or Password");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
 
         }
     }
