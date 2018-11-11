8e4cb5de8701bbcdd466f863bfa24fc0bf0229a7
Benjamin Falsetto <32547110+BenjaminFalsetto@users.noreply.github.com>
2018-11-11 17:51:21 -0500

    Some other stuff

--- SimplifiedBenF/SimplifiedBenF/SimplifiedForm.Designer.cs
+++ SimplifiedBenF/SimplifiedBenF/SimplifiedForm.Designer.cs
@@ -31,6 +31,14 @@
             this.btnNewGame = new System.Windows.Forms.Button();
             this.lstPlayerCards = new System.Windows.Forms.ListBox();
             this.lblPlayerCards = new System.Windows.Forms.Label();
+            this.btnHit = new System.Windows.Forms.Button();
+            this.mnuMenu = new System.Windows.Forms.MenuStrip();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniNewGame = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblPlayerTotal = new System.Windows.Forms.Label();
+            this.btnStand = new System.Windows.Forms.Button();
+            this.mnuMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnNewGame
@@ -46,7 +54,7 @@
             // lstPlayerCards
             // 
             this.lstPlayerCards.FormattingEnabled = true;
-            this.lstPlayerCards.Location = new System.Drawing.Point(114, 160);
+            this.lstPlayerCards.Location = new System.Drawing.Point(117, 188);
             this.lstPlayerCards.Name = "lstPlayerCards";
             this.lstPlayerCards.Size = new System.Drawing.Size(120, 95);
             this.lstPlayerCards.TabIndex = 1;
@@ -54,22 +62,92 @@
             // lblPlayerCards
             // 
             this.lblPlayerCards.AutoSize = true;
-            this.lblPlayerCards.Location = new System.Drawing.Point(114, 141);
+            this.lblPlayerCards.Location = new System.Drawing.Point(114, 172);
             this.lblPlayerCards.Name = "lblPlayerCards";
             this.lblPlayerCards.Size = new System.Drawing.Size(66, 13);
             this.lblPlayerCards.TabIndex = 2;
             this.lblPlayerCards.Text = "Player Cards";
             // 
+            // btnHit
+            // 
+            this.btnHit.Location = new System.Drawing.Point(117, 131);
+            this.btnHit.Name = "btnHit";
+            this.btnHit.Size = new System.Drawing.Size(59, 23);
+            this.btnHit.TabIndex = 3;
+            this.btnHit.Text = "Hit";
+            this.btnHit.UseVisualStyleBackColor = true;
+            this.btnHit.Click += new System.EventHandler(this.btnHit_Click);
+            // 
+            // mnuMenu
+            // 
+            this.mnuMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile});
+            this.mnuMenu.Location = new System.Drawing.Point(0, 0);
+            this.mnuMenu.Name = "mnuMenu";
+            this.mnuMenu.Size = new System.Drawing.Size(502, 24);
+            this.mnuMenu.TabIndex = 4;
+            this.mnuMenu.Text = "menuStrip1";
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniNewGame,
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Text = "File";
+            // 
+            // mniNewGame
+            // 
+            this.mniNewGame.Name = "mniNewGame";
+            this.mniNewGame.Size = new System.Drawing.Size(180, 22);
+            this.mniNewGame.Text = "New Game";
+            this.mniNewGame.Click += new System.EventHandler(this.mniNewGame_Click);
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(180, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // lblPlayerTotal
+            // 
+            this.lblPlayerTotal.AutoSize = true;
+            this.lblPlayerTotal.Location = new System.Drawing.Point(117, 290);
+            this.lblPlayerTotal.Name = "lblPlayerTotal";
+            this.lblPlayerTotal.Size = new System.Drawing.Size(59, 13);
+            this.lblPlayerTotal.TabIndex = 5;
+            this.lblPlayerTotal.Text = "Player total";
+            // 
+            // btnStand
+            // 
+            this.btnStand.Location = new System.Drawing.Point(178, 131);
+            this.btnStand.Name = "btnStand";
+            this.btnStand.Size = new System.Drawing.Size(59, 23);
+            this.btnStand.TabIndex = 6;
+            this.btnStand.Text = "Stand";
+            this.btnStand.UseVisualStyleBackColor = true;
+            this.btnStand.Click += new System.EventHandler(this.btnStand_Click);
+            // 
             // frmSimplified
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(502, 370);
+            this.Controls.Add(this.btnStand);
+            this.Controls.Add(this.lblPlayerTotal);
+            this.Controls.Add(this.btnHit);
             this.Controls.Add(this.lblPlayerCards);
             this.Controls.Add(this.lstPlayerCards);
             this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.mnuMenu);
+            this.MainMenuStrip = this.mnuMenu;
             this.Name = "frmSimplified";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmSimplified_Load);
+            this.mnuMenu.ResumeLayout(false);
+            this.mnuMenu.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -80,6 +158,13 @@
         private System.Windows.Forms.Button btnNewGame;
         private System.Windows.Forms.ListBox lstPlayerCards;
         private System.Windows.Forms.Label lblPlayerCards;
+        private System.Windows.Forms.Button btnHit;
+        private System.Windows.Forms.MenuStrip mnuMenu;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniNewGame;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+        private System.Windows.Forms.Label lblPlayerTotal;
+        private System.Windows.Forms.Button btnStand;
     }
 }
 
--- SimplifiedBenF/SimplifiedBenF/SimplifiedForm.cs
+++ SimplifiedBenF/SimplifiedBenF/SimplifiedForm.cs
@@ -14,6 +14,7 @@ namespace SimplifiedBenF
     {
         //declare variables
         int playerTotal, dealerTotal, playerNextCard, dealerNextCard;
+        string returnToPlayer, returnToComputer;
         Random randomCardGenerator;
 
         public frmSimplified()
@@ -22,24 +23,192 @@ namespace SimplifiedBenF
             randomCardGenerator = new Random();
         }
 
-        //declare global functions
+        private void mniNewGame_Click(object sender, EventArgs e)
+        {
+            NewGame();
+        }
+
+        private void frmSimplified_Load(object sender, EventArgs e)
+        {
+            btnHit.Visible = false;
+            btnStand.Visible = false;
+            lblPlayerCards.Visible = false;
+            lstPlayerCards.Visible = false;
+            lstPlayerCards.Items.Clear();
+            lblPlayerTotal.Visible = false;
+        }
+
+        //declare local functions
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        
         private void NewGame()
         {
-            btnNewGame.Visible = false;
             lstPlayerCards.Items.Clear();
+            btnNewGame.Visible = false;
+            btnHit.Visible = true;
+            btnStand.Visible = true;
+            lblPlayerCards.Visible = true;
+            lstPlayerCards.Visible = true;
+            lblPlayerTotal.Text = ("Player total");
+            lblPlayerTotal.Visible = true;
+            PlayerHit();
+            PlayerHit();
+            UpdatePlayerTotal();
+            DealerHit();
+            DealerHit();
+        }
+
+        private void btnHit_Click(object sender, EventArgs e)
+        {
+            PlayerHit();
+            UpdatePlayerTotal();
         }
 
         private void PlayerHit()
         {
             playerNextCard = randomCardGenerator.Next(1, 10 + 1);
             lstPlayerCards.Items.Add(playerNextCard);
+            playerTotal = playerTotal + playerNextCard;
+            CheckPlayerTotal();
         }
 
-
+        private void btnStand_Click(object sender, EventArgs e)
+        {
+            btnHit.Enabled = false;
+            btnStand.Enabled = false;
+            CheckDealerHit();
+        }
 
         private void btnNewGame_Click(object sender, EventArgs e)
         {
             NewGame();
+        }
+
+        private void UpdatePlayerTotal()
+        {
+            lblPlayerTotal.Text = ("Player total" + playerTotal);
+        }
+
+        private void CheckPlayerTotal()
+        {
+            if (playerTotal == 21)
+            {
+                MessageBox.Show("Blackjack!");
+                btnHit.Visible = false;
+                btnStand.Visible = false;
+                lstPlayerCards.Visible = false;
+                lblPlayerCards.Visible = false;
+                lblPlayerTotal.Visible = false;
+                lstPlayerCards.Items.Clear();
+                playerTotal = 0;
+                dealerTotal = 0;
+                btnNewGame.Visible = true;
+            }
+            else if (playerTotal > 21)
+            {
+                PlayerBust();
+            }
+        }
+        private void PlayerBust()
+        {
+            MessageBox.Show("Player Bust");
+            btnHit.Visible = false;
+            btnStand.Visible = false;
+            lstPlayerCards.Visible = false;
+            lblPlayerCards.Visible = false;
+            lblPlayerTotal.Visible = false;
+            lstPlayerCards.Items.Clear();
+            playerTotal = 0;
+            dealerTotal = 0;
+            btnNewGame.Visible = true;
+        }
+
+        private void CheckDealerHit()
+        {
+            if (dealerTotal <= 16)
+            {
+                DealerHit();
+            }
+            else
+            {
+                DealerFinished();
+            }
+        }
+
+        private void DealerHit()
+        {
+            dealerNextCard = randomCardGenerator.Next(1, 10 + 1);
+            dealerTotal = dealerTotal + dealerNextCard;
+            CheckDealerTotal();
+            CheckDealerHit();
+        }
+        
+        private void DealerBust()
+        {
+            MessageBox.Show("Dealer Bust");
+            btnHit.Visible = false;
+            btnStand.Visible = false;
+            lstPlayerCards.Visible = false;
+            lblPlayerCards.Visible = false;
+            lblPlayerTotal.Visible = false;
+            lstPlayerCards.Items.Clear();
+            playerTotal = 0;
+            dealerTotal = 0;
+            btnNewGame.Visible = true;
+        }
+        private void CheckDealerTotal()
+        {
+            if (dealerTotal == 21)
+            {
+                MessageBox.Show("Dealer has blackjack");
+                btnHit.Visible = false;
+                btnStand.Visible = false;
+                lstPlayerCards.Visible = false;
+                lblPlayerCards.Visible = false;
+                lblPlayerTotal.Visible = false;
+                lstPlayerCards.Items.Clear();
+                playerTotal = 0;
+                dealerTotal = 0;
+                btnNewGame.Visible = true;
+            }
+            else if (dealerTotal > 21)
+            {
+                DealerBust();
+            }
+        }
+
+        private void DealerFinished()
+        {
+            if (playerTotal > dealerTotal)
+            {
+                MessageBox.Show("Player wins");
+                btnHit.Visible = false;
+                btnStand.Visible = false;
+                lstPlayerCards.Visible = false;
+                lblPlayerCards.Visible = false;
+                lblPlayerTotal.Visible = false;
+                lstPlayerCards.Items.Clear();
+                playerTotal = 0;
+                dealerTotal = 0;
+                btnNewGame.Visible = true;
+            }
+            else if (dealerTotal > playerTotal)
+            {
+                MessageBox.Show("Dealer wins");
+                btnHit.Visible = false;
+                btnStand.Visible = false;
+                lstPlayerCards.Visible = false;
+                lblPlayerCards.Visible = false;
+                lblPlayerTotal.Visible = false;
+                lstPlayerCards.Items.Clear();
+                playerTotal = 0;
+                dealerTotal = 0;
+                btnNewGame.Visible = true;
+            }
         }
     }
 }
